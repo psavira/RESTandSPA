ceb484537142657371d5c9b955d256275d5399fc
psavira <savi2586@stthomas.edu>
2020-12-14 14:32:00 -0600

    done 3 GET

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-// Built-in Node.js modules
+//modules
 let path = require('path');
-
-// NPM modules
 let express = require('express');
 let sqlite3 = require('sqlite3');
-
+var fs = require("fs");
 
 let app = express();
 let port = 8000;
@@ -30,25 +28,177 @@ app.use(express.static(public_dir));
 // REST API: GET /codes
 // Respond with list of codes and their corresponding incident type
 app.get('/codes', (req, res) => {
-    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+    res.header();
+    //let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
     
-    res.status(200).type('json').send({});
+    var codeArray = [];
+    var query = "SELECT * FROM Codes";
+
+    if (req.query.code !== undefined) { //codes are specified
+        var codeDataRows = req.query.code.split(",");
+
+        query += " WHERE code=?";
+        for (let i=1; i<codeDataRows.length; i++) {
+            query += " OR code=?"
+        }
+        db.each(query, codeDataRows, (err, row) => {
+            codeObj = {};
+            codeObj["code"] = row.code;
+            codeObj["type"] = row.incident_type;
+            codeArray.push(codeObj);            
+        }, ()=> {
+            res.type("json").send(codeArray);
+            //res.type("json").send(JSON.stringify(codeArray, null, 1));
+        });
+
+
+    } else { //codes are NOT specified
+        db.each(query, (err, row) => {
+            codeObj = {};
+            codeObj["code"] = row.code;
+            codeObj["type"] = row.incident_type;
+            codeArray.push(codeObj);            
+        }, ()=> {
+            res.type("json").send(codeArray);
+            //res.type("json").send(JSON.stringify(codeArray, null, 1));
+        });
+    }
 });
 
 // REST API: GET /neighborhoods
 // Respond with list of neighborhood ids and their corresponding neighborhood name
 app.get('/neighborhoods', (req, res) => {
-    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+    res.header();
+    //let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
 
-    res.status(200).type('json').send({});
+    var neighborhoodArray = []; //needs to be a JSON object instead of a JSON array
+    var query = "SELECT * FROM Neighborhoods";
+
+    if (req.query.id !== undefined) { //neighborhoods are specified
+        var neighborhoodDataRows = req.query.id.split(",");
+
+        query += " WHERE neighborhood_number=?";
+        for (let i=1; i<neighborhoodDataRows.length; i++) {
+            query += " OR neighborhood_number=?"
+        }
+        console.log(query);
+        db.each(query, neighborhoodDataRows, (err, row) => {
+            neighborhoodObj = {};
+            neighborhoodObj["id"] = row.neighborhood_number;
+            neighborhoodObj["name"] = row.neighborhood_name;
+            neighborhoodArray.push(neighborhoodObj);            
+        }, ()=> {
+            res.type("json").send(neighborhoodArray);
+            //res.type("json").send(JSON.stringify(neighborhoodArray, null, 1));
+        });
+
+
+    } else { //codes are NOT specified
+        db.each(query, neighborhoodDataRows, (err, row) => {
+            neighborhoodObj = {};
+            neighborhoodObj["id"] = row.neighborhood_number;
+            neighborhoodObj["name"] = row.neighborhood_name;
+            neighborhoodArray.push(neighborhoodObj);            
+        }, ()=> {
+            res.type("json").send(neighborhoodArray);
+            //res.type("json").send(JSON.stringify(neighborhoodArray, null, 1));
+        });
+    }
 });
 
 // REST API: GET/incidents
 // Respond with list of crime incidents
-app.get('/incidents', (req, res) => {
-    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+app.get('/incidents', (req, res) => { //NOT QUITE WORKING YET
+    res.header();
+    //let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+    
+    //setting limit to 1000 by default
+    var limit=1000;
+    if(req.query.limit!==undefined) {
+		limit=parseInt(req.query.limit);
+    }
 
-    res.status(200).type('json').send({});
+    var incidentArray = [];
+    //var query = "SELECT * FROM Incidents ORDER BY date_time DESC LIMIT ?"; //order by time in descending order
+    var whereQuery = "";
+
+    //start date
+    if(req.query.start_date!==undefined) {
+		if(whereQuery==="") {
+			whereQuery = " WHERE (date_time >= "+"'"+ req.query.start_date+"T00:00:00"+"')";
+		} else {
+			whereQuery = whereQuery + " AND (date_time >= "+"'"+req.query.start_date+"T"+"00:00:00')";
+		}
+    }
+    
+    //end date
+    if(req.query.end_date!==undefined) {
+		if(whereQuery==="") {
+			whereQuery = " WHERE ( date_time <= "+"'"+ req.query.end_date+"T"+"23:59:59')";
+		} else {
+			whereQuery = whereQuery + " AND ( date_time <= "+ "'"+req.query.end_date+"T"+"23:59:59')";
+		}
+	}
+
+    //code
+    if(req.query.code!==undefined) {
+        var codes=req.query.code.split(",");
+        if(whereQuery==="") {
+            whereQuery = " WHERE (code="+codes[0];;
+        } else {
+            whereQuery= whereQuery + " AND (code="+codes[0];
+        }
+		for(let i =1;i<codes.length;i++) {
+			whereQuery = whereQuery+  " OR code="+codes[i];
+		}
+		whereQuery = whereQuery+")";
+    }
+
+    //grid
+    if(req.query.grid!==undefined) {
+		var grids= req.query.grid.split(",");
+		if(whereQuery==="") {
+			whereQuery = " WHERE (police_grid="+grids[0];
+		} else {
+			whereQuery = whereQuery+" AND (police_grid="+grids[0];
+		}
+		for(let i=1;i<grids.length;i++) {
+			whereQuery = whereQuery+ " OR police_grid="+grids[i];
+		}
+		whereQuery = whereQuery+")";
+    }
+    
+    //neighborhood
+    if(req.query.neighborhood!==undefined) {
+		var neighborhood= req.query.neighborhood.split(",");
+		if(whereQuery==="") {
+			whereQuery= "WHERE (neighborhood_number="+neighborhood[0];
+		} else {			
+			whereQuery = whereQuery + " AND (neighborhood_number="+neighborhood[0];
+		}
+		for(let i =1;i<neighborhood.length;i++){
+			whereQuery = whereQuery + " OR neighborhood_number="+ neighborhood[i];
+		}
+		whereQuery = whereQuery+")";
+	}
+
+    console.log(whereQuery);
+    db.each("SELECT * FROM Incidents" + whereQuery + "ORDER BY date_time DESC LIMIT ?", [limit], (err, row) => {
+        incidentObj = {};
+        incidentObj["case_number"] = row.case_number;
+        var dateAndTime = row.date_time.split("T");
+        incidentObj["date"] = dateAndTime[0];
+        incidentObj["time"] = dateAndTime[1];
+        incidentObj["code"] = row.code;
+        incidentObj["incident"] = row.incident;
+        incidentObj["police_grid"] = row.police_grid;
+        incidentObj["neighborhood_number"] = row.neighborhood_number;
+        incidentObj["block"] = row.block;
+        incidentArray.push(incidentObj);            
+    }, ()=> {
+        res.type("json").send(incidentArray);
+        //res.type("json").send(JSON.stringify(incidentArray, null, 1));
+    });
 });
 
 // REST API: PUT /new-incident
